06b062e1a50870276d98ca0d6233f2694c6f4eeb
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2023-04-20 13:34:04 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { HomePage } from "./Components/HomePage";
+import Temp from "./Components/Temp";
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      <Temp />
     </div>
   );
 }
---
+++ src/Components/Temp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Temp(props) {
+    return (
+        <div style={{width:"412px", height:"1056px", backgroundColor:"red"}}>
+            giihj
+        </div>
+    );
+}
+
+export default Temp;
